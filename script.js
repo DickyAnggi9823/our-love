66cdd5517399e8760c4ddce14cdfe6c74bf3aec0
DickyAnggi9823 <dickysebastian115@gmail.com>
2026-01-11 21:16:50 +0700

    script.js

--- script.js
+++ script.js
@@ -1,62 +1,10 @@
-const tapScreen = document.getElementById("tapScreen");
-const content = document.getElementById("content");
-const music = document.getElementById("music");
-const gallery = document.getElementById("gallery");
+const overlay = document.getElementById("overlay");
+const music = document.getElementById("bgMusic");
 
-const API = "https://our-love-backend.onrender.com";
-
-// TAP TO START
-tapScreen.addEventListener("click", function () {
-    tapScreen.style.display = "none";
-    content.classList.remove("hidden");
-    music.play();
-    loadPhotos();
+overlay.addEventListener("click", () => {
+    music.play().then(() => {
+        overlay.style.display = "none";
+    }).catch(error => {
+        console.log("Music play blocked:", error);
+    });
 });
-
-// LOAD PHOTOS
-function loadPhotos() {
-    fetch(API + "/photos")
-        .then(function (res) {
-            return res.json();
-        })
-        .then(function (data) {
-            gallery.innerHTML = "";
-            data.forEach(function (img) {
-                const image = document.createElement("img");
-                image.src = API + img;
-                gallery.appendChild(image);
-            });
-        })
-        .catch(function (err) {
-            console.error(err);
-        });
-}
-
-// UPLOAD PHOTO
-function uploadPhoto() {
-    const input = document.getElementById("photoInput");
-
-    if (!input.files || !input.files[0]) {
-        alert("Pilih foto dulu 💗");
-        return;
-    }
-
-    const formData = new FormData();
-    formData.append("photo", input.files[0]);
-
-    fetch(API + "/upload", {
-        method: "POST",
-        body: formData
-    })
-        .then(function (res) {
-            return res.json();
-        })
-        .then(function () {
-            input.value = "";
-            loadPhotos();
-        })
-        .catch(function (err) {
-            console.error(err);
-        });
-}
-
